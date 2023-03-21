513cb692c6ae6ca42c79bbfd54320ea64efb0e2d
danielbeltrano <82431546+omardbeltrano@users.noreply.github.com>
2023-03-20 20:41:10 -0500

    First commit. properties configuration and Todo Entity setup

---
+++ src/main/java/com/app/crud/todo/CrudTodoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.app.crud.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTodoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTodoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/crud/todo/entity/Todo.java
@@ -0,0 +1,71 @@
+package com.app.crud.todo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//Assign table name
+@Table(name = "todo")
+public class Todo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Column(name = "task", nullable = false, length = 50)
+	private String task;
+	
+	@Column(name = "date_task", nullable = false)
+	//Pay attention with type of date's library
+	private Date dateTask;
+	
+	//Empty constructor to Java beans
+	public Todo() {
+		
+	}
+
+	public Todo(Long id, String task, Date dateTask) {
+		Id = id;
+		this.task = task;
+		this.dateTask = dateTask;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public Date getDateTask() {
+		return dateTask;
+	}
+
+	public void setDateTask(Date dateTask) {
+		this.dateTask = dateTask;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [Id=" + Id + ", task=" + task + ", dateTask=" + dateTask + "]";
+	}
+	
+	
+	
+	
+}
